6b780b1336018f43b68e2579a8696c1214433d15
apurba4u <apurba185loser@gmail.com>
2026-02-21 23:49:48 +0600

    2nd step done

--- machine.js
+++ machine.js
@@ -1,1 +1,14 @@
-console.log('connected');
+function showOnly(id) {
+  const interview = document.querySelector("#interview")
+  const all = document.querySelector("#all")
+  const rejected = document.querySelector("#rejected")
+  
+  // shobai k hide kore dao
+  interview.classList.add('hidden')
+  all.classList.add('hidden')
+  rejected.classList.add('hidden')
+
+  // id wala ta ke show koro tumi
+  const selected = document.querySelector(`#${id}`);
+  selected.classList.remove('hidden')
+}
